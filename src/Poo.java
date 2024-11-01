4237e8df7c683b0aaf0028ef2a81c21832fca066
Abdelouahed06 <kaabwd@gmail.com>
2024-11-01 21:04:19 +0100

    call method inside anther method

--- src/Poo.java
+++ src/Poo.java
@@ -1,5 +1,8 @@
 public class Poo {
-    public static void main(String[] args) {
+    static void myMethod() {
         System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        myMethod();
     }
 }
