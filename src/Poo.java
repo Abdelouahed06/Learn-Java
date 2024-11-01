2a308a1e81a6fbaff652f7064138426ac5445996
Abdelouahed06 <kaabwd@gmail.com>
2024-11-01 21:42:25 +0100

    return in method

--- src/Poo.java
+++ src/Poo.java
@@ -3,19 +3,24 @@ public class Poo {
 //        System.out.println("Welcome " + fname + "your age is " + age);
 //    }
 
-    static void checkAge(int age) {
-        if (age < 18){
-            System.out.println("you age must be at least 18 years");
-        }
-        else {
-            System.out.println("welcome !");
-        }
+//    static void checkAge(int age) {
+//        if (age < 18){
+//            System.out.println("you age must be at least 18 years");
+//        }
+//        else {
+//            System.out.println("welcome !");
+//        }
+//    }
+
+    static int myfunc(int x){
+        return 5 + x ;
     }
     public static void main(String[] args) {
 //        myMethod("abde",20);
 //        myMethod("ali",42);
 //        myMethod("hhafid",30);
-        checkAge(14);
-        checkAge(25);
+//        checkAge(14);
+//        checkAge(25);
+        System.out.println(myfunc(5));
     }
 }
